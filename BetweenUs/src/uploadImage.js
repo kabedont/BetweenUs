0ff31727a3a69ccb3e702e6d513eb98d2838d40b
kabedont <jjenniferjosephina@outlook.com>
2026-04-01 23:32:59 +0800

    Add client-side image compression
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- BetweenUs/src/uploadImage.js
+++ BetweenUs/src/uploadImage.js
@@ -1,11 +1,21 @@
 import {supabase} from './supabaseClient'
+import imageCompression from 'browser-image-compression';
 
 async function uploadImage(file){
     const fileName = `${Date.now()}_${file.name}`
+
+    const options = {
+        maxSizeMB: 0.5,
+        maxWidthOrHeight: 1200,
+        useWebWorker: true
+    };
+
+    const compressedFile = await imageCompression(file,options);
+
     const {data, error} = await supabase
         .storage
         .from('galleries')
-        .upload(fileName, file)
+        .upload(fileName, compressedFile)
     if (error){
         console.error(error)
         return null
