68565216628c7d72e631b42d9726c06e69f042d5
kabedont <jjenniferjosephina@outlook.com>
2026-04-07 09:42:39 +0800

    Insert expires_at into Supabase
    Signed-off-by: kabedont <jjenniferjosephina@outlook.com>

--- BetweenUs/src/saveGallery.js
+++ BetweenUs/src/saveGallery.js
@@ -1,21 +1,21 @@
 import {supabase} from './supabaseClient'
 
 async function saveGallery(gridSize, imageUrls, descriptions, expiryDays){
+    let expires_at = null;
+    if (expiryDays !== null && expiryDays > 0){
+        const expiryDate = new Date();  //gets current time
+        expiryDate.setDate(expiryDate.getDate() + expiryDays)
+        expires_at = expiryDate.toISOString();  //formats for supabase
+    }
+    
     const{data, error} = await supabase
         .from('galleries')
-        .insert([{grid_size: gridSize, image_urls: imageUrls, descriptions: descriptions}])
+        .insert([{grid_size: gridSize, image_urls: imageUrls, descriptions: descriptions, expires_at: expires_at}])
         .select() //returns inserted row with id
     
     if(error){
         console.error("Error saving gallery: ", error)
         return null
-    }
-
-    let expires_at = null;
-    if (expires_at !== null && expires_at > 0){
-        const expiryDate = new Date();  //gets current time
-        expiryDate.setDate(expiryDate.getDate() + expiryDays)
-        expires_at = expiryDate.toISOString();  //formats for supabase
     }
 
     return data[0].id //returns unique gallery ID
